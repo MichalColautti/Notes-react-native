922a0ca17fc0c2e3a923783c7b4a51c1cd78be71
MichalC <m.colautti@o2.pl>
2025-07-16 20:25:38 +0200

    Added home screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+const RootLayout = () => {
+  return (
+    <Stack 
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f35d00ff'
+        },
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: 'bold',
+          color: '#000000'
+        },
+        contentStyle: {
+          paddingHorizontal: 10,
+          paddingTop: 10,
+          backgroundColor: '#ffffff'
+        }
+        
+    }}>
+      <Stack.Screen name='index' options={{ title: 'Home'}}/>
+    </Stack>
+  )
 }
+
+export default RootLayout
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function Index() {
+const HomeScreen = () => {
+  const router = useRouter();
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      style={styles.container}>
+      <Text style={styles.title}>Notes</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push('/notes')}
+      >
+        <Text style={styles.buttonText}>Get started</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f8f9fa',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333333'
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+})
+
+export default HomeScreen
