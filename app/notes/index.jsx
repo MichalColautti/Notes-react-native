7500060a55af8f1dc14c7e457664cfcc51b8001a
MichalC <m.colautti@o2.pl>
2025-07-28 17:16:22 +0200

    Add save and cancel buttons to modal

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -52,6 +52,17 @@ const NoteScreen = () => {
                         value={newNote}
                         onChangeText={setNewNote}
                     />
+                    <View style={styles.modalButtonsRow}>
+                        <TouchableOpacity 
+                            style={styles.cancelButton}
+                            onPress={ () => setModalVisible(false)}
+                        >
+                            <Text style={styles.cancelButtonText}>Cancel</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.saveNoteButton}>
+                            <Text style={styles.saveNoteText}>Add note</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -115,7 +126,38 @@ const styles = StyleSheet.create({
         padding: 10,
         fontSize: 16,
         marginBottom: 15,
-    }
+    },
+    modalButtonsRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    cancelButton: {
+        borderWidth: 1,
+        borderColor: '#000000',
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#aaaaaa',
+        flex: 1,
+        alignItems: 'center',
+        marginRight: 10,
+    },
+    cancelButtonText: {
+        fontSize: 16,
+        color: '#000000',
+    },
+    saveNoteButton: {
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#000000',
+        borderRadius: 4,
+        backgroundColor: '#007bff',
+        flex: 1,
+        alignItems: 'center',
+    },
+    saveNoteText: {
+        fontSize: 16,
+        color: '#ffffff'
+    },
 })
 
 export default NoteScreen;
